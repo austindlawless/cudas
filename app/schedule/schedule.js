1635a22858f28eeb9a9723f8b4199733dfee5c61
Austin Lawless <austindlawless@gmail.com>
2016-02-21 23:46:48 -0600

    removed roster background shading, added standings link to cedar park page

--- app/schedule/schedule.js
+++ app/schedule/schedule.js
@@ -12,6 +12,5 @@ angular.module('myApp.schedule', ['ngRoute'])
 	.controller('ScheduleCtrl', ['$scope', '$http', function ($scope, $http) {
 		$http.get("https://eg5f5pcd8i.execute-api.us-east-1.amazonaws.com/prod/cudas_getSchedule").then(function (response) {
 			$scope.games = response.data.Item.games;
-			console.log($scope);
 		});
 	}]);
