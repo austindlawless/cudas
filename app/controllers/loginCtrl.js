d5696fb814120d72e3a4797f6c179e1a8871f054
Austin Lawless <austindlawless@gmail.com>
2016-03-14 01:56:54 -0500

    started adding auth request

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -7,5 +7,7 @@ angular.module('myApp.roster', ['ngRoute'])
 		$scope.login = function () {
 			console.log("username=" + $scope.username);
 			console.log("password=" + $scope.password);
+			var auth_token = $http.post('https://eg5f5pcd8i.execute-api.us-east-1.amazonaws.com/prod/getAuthToken', {"username": $scope.username, "password": $scope.password});
+			conosle.log("auth_token=" + auth_token);
 		};
 	}]);
