fbbff6c4bf731a251d3d725dda1977502c3d645f
Austin Lawless <austin.lawless@bancvue.com>
2016-06-10 12:49:32 -0500

    [AUTH] added get message lambda and get roster lambda, updated home page to show message board

--- app/home/home.js
+++ app/home/home.js
@@ -2,13 +2,15 @@
 
 angular.module('myApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/home', {
+			templateUrl: 'home/home.html',
+			controller: 'HomeCtrl'
+		});
+	}])
 
-.controller('HomeCtrl', [function() {
-
-}]);
+	.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+		$http.get("https://eg5f5pcd8i.execute-api.us-east-1.amazonaws.com/prod/cudas_getMessages").then(function (response) {
+			$scope.messages = response.data.Items;
+		});
+	}]);
