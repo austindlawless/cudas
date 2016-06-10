a2a46268cbae53ab2b5d0fe098b186d1bd3bd149
Austin Lawless <austin.lawless@bancvue.com>
2016-06-10 17:23:13 -0500

    [AUTH] message post now includes date, posts are now reverse ordered by date

--- lambda/postMessage/lambda_function.py
+++ lambda/postMessage/lambda_function.py
@@ -3,16 +3,15 @@ from __future__ import print_function
 import boto3
 import json
 import uuid
+import datetime
 
 print('Loading function')
 
 def lambda_handler(event, context):
 	print("Received event: " + json.dumps(event, indent=2))
 	message = event["message"]
-	print(message)
 	if (message["content"]) :
 		id = str(uuid.uuid4())
-		item = {'id':id, 'content':message["content"]}
-		print(json.dumps(item))
+		item = {'id':id, 'content':message["content"], 'date':'{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())}
 		messageTable = boto3.resource('dynamodb').Table('cudas_message')
-		print(messageTable.put_item(Item=item))
+		return messageTable.put_item(Item=item)
