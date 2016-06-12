95dabd30778ec30498b98f44586773fa37639288
Austin Lawless <austin.lawless@bancvue.com>
2016-06-11 21:31:29 -0500

    [AUTH] auth token is now included on all requests from the app, need to go turn on options methods for other resources

--- app/app.js
+++ app/app.js
@@ -13,66 +13,62 @@ angular.module('myApp', [
 	
 	.config(['$stateProvider','$routeProvider', '$httpProvider', function ($stateProvider, $routeProvider, $httpProvider) {
 		$stateProvider
-        .state('schedule', {
-            url: '/schedule',
-            templateUrl: 'schedule/schedule.html',
-            controller: 'ScheduleCtrl'
-        });
+		.state('schedule', {
+			url: '/schedule',
+			templateUrl: 'schedule/schedule.html',
+			controller: 'ScheduleCtrl'
+		});
 
 		$routeProvider.otherwise({redirectTo: '/home'});
 		
 		$httpProvider.interceptors.push('APIInterceptor');
 	}])
 
-	.service('APIInterceptor', function ($rootScope, UserService) {
-        var service = this;
+	.service('APIInterceptor', ['$rootScope', 'UserService', function ($rootScope, UserService) {
+		var service = this;
 
-        service.request = function (config) {
-			var currentUser = UserService.getCurrentUser(),
-				access_token = currentUser ? currentUser.access_token : null;
-
+		service.request = function (config) {
+			var currentUser = UserService.getCurrentUser();
+			var access_token = currentUser ? currentUser.auth_token : null;
 			if (access_token) {
-				config.headers.authorization = access_token;
+				config.headers.Authorization = access_token;
 			}
 			return config;
-        };
+		};
 
-        service.responseError = function (response) {
+		service.responseError = function (response) {
 			if (response.status === 401) {
 				$rootScope.$broadcast('unauthorized');
 			}
 			return response;
-        };
-    })
+		};
+	}])
 
-	.service('UserService', function (store) {
-		console.log('entered UserService');
-        var service = this,
-        currentUser = null;
+	.service('UserService', ['store', function (store) {
+		var service = this,
+		currentUser = null;
 
 		service.setCurrentUser = function (user) {
-            currentUser = user;
-            store.set('user', user);
-            return currentUser;
-        };
+			currentUser = user;
+			store.set('user', user);
+			return currentUser;
+		};
 
-        service.getCurrentUser = function () {
-            if (!currentUser) {
-                currentUser = store.get('user');
-            }
-            return currentUser;
-        };
-	})
+		service.getCurrentUser = function () {
+			if (!currentUser) {
+				currentUser = store.get('user');
+			}
+			return currentUser;
+		};
+	}])
 	
-	.controller('MainCtrl', ['$rootScope', '$scope', '$http', 'UserService', function ($rootScope, $scope, $http, UserService) {
-		console.log("registered the MainCtrl");
-		
+	.controller('MainCtrl', ['$rootScope', '$scope', '$http', '$log', 'UserService', function ($rootScope, $scope, $http, $log, UserService) {		
 		var main = this;
 		
-		console.log("currentUser=" + JSON.stringify(UserService.getCurrentUser()));
+		$log.debug("currentUser=" + JSON.stringify(UserService.getCurrentUser()));
 
 		function logout() {
-			console.log('Logging out ' + JSON.stringify(main.currentUser));
+			$log.debug('Logging out ' + JSON.stringify(main.currentUser));
 			main.currentUser = null;
 			UserService.setCurrentUser(null);
 		}
@@ -83,7 +79,7 @@ angular.module('myApp', [
 		$scope.main = main;
 			
 		$scope.login = function (user) {
-			console.log("user=" + JSON.stringify(user));
+			$log.debug("user=" + JSON.stringify(user));
 			
 			$http({
 				method: "POST",
@@ -91,7 +87,7 @@ angular.module('myApp', [
 				data: {"username": user.username, "password": user.password},
 				headers: {}
 			}).then(function (response) {
-				console.log("response.data=" + JSON.stringify(response.data));
+				$log.debug("response.data=" + JSON.stringify(response.data));
 				if (response.data) {
 					user.auth_token = response.data.auth_token;
 					UserService.setCurrentUser(user);
