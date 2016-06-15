77106a9be909895f79a51c11e118178702683c8f
Austin Lawless <austindlawless@gmail.com>
2016-06-14 23:16:28 -0500

    message posts now pull the user from the auth token

--- lambda/postMessage/lambda_function.py
+++ lambda/postMessage/lambda_function.py
@@ -4,14 +4,26 @@ import boto3
 import json
 import uuid
 import datetime
+import jwt
 
 print('Loading function')
 
+
 def lambda_handler(event, context):
-	print("Received event: " + json.dumps(event, indent=2))
-	message = event["message"]
-	if (message["content"]) :
-		id = str(uuid.uuid4())
-		item = {'id':id, 'content':message["content"], 'date':'{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())}
-		messageTable = boto3.resource('dynamodb').Table('cudas_message')
-		return messageTable.put_item(Item=item)
+    print("Received event: " + json.dumps(event, indent=2))
+    message = event["data"]["message"]
+    auth_token = event["auth_token"]
+    try:
+        payload = jwt.decode(auth_token, verify=False)
+    except jwt.InvalidTokenError:
+        raise Exception('Unauthorized')
+
+    username = payload["username"]
+    print('username: ' + username)
+
+    if (message["content"]):
+        id = str(uuid.uuid4())
+        date = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        item = {'id': id, 'content': message["content"], 'date': date, 'username': username}
+        messageTable = boto3.resource('dynamodb').Table('cudas_message')
+        return messageTable.put_item(Item=item)
