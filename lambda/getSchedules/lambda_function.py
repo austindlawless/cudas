6378f7739e53296bda22426d818b8738476087d7
Austin Lawless <austindlawless@gmail.com>
2017-10-22 08:31:55 -0500

    standings link update

---
+++ lambda/getSchedules/lambda_function.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import boto3
+import json
+
+def lambda_handler(event, context):
+    dynamo = boto3.resource('dynamodb').Table('cudas_schedule')
+    return dynamo.get_item(Key={'season':'20172'})
